dd220f0d3a7fad3450005e1dc6d23b144b8b3ef8
chenzengqing <chenzengqing@utan.com>
2017-02-17 11:45:21 +0800

    添加selector测试页面

--- app/src/main/java/com/ui/test/androiduistyletest/MainActivity.java
+++ app/src/main/java/com/ui/test/androiduistyletest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ui.test.androiduistyletest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
+    }
+
+    public void goToShape(View view) {
+        startActivity(new Intent(MainActivity.this,ShapeActivity.class));
+    }
+
+    public void goToSelector(View view) {
+        startActivity(new Intent(MainActivity.this,SelectorActivity.class));
     }
 }
